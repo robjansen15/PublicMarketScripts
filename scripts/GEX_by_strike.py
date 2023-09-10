f978bb5630e4221561c8378b282eb7fb55073a78
Rob <robjansen15@gmail.com>
2023-09-10 18:29:23 -0400

    Fix GEX computation

--- scripts/GEX_by_strike.py
+++ scripts/GEX_by_strike.py
@@ -12,7 +12,7 @@ def scrape_data(ticker='SPY', save_to_file=True):
     # Request data and save it to file
     try:
         data = requests.get(
-            f"https://cdn.cboe.com/api/global/delayed_quotes/options/{ticker}.json"
+            f"https://cdn.cboe.com/api/global/delayed_quotes/options/_{ticker}.json"
         )
         
         if(save_to_file):
@@ -43,34 +43,39 @@ def fix_option_data(data):
     data["expiration"] = pd.to_datetime(data["expiration"], format="%y%m%d")
     return data
   
-def compute_weighted_gamma(ticker, num_of_strikes):
+def compute_GEX(ticker, num_of_strikes):
     spot_price, option_data = scrape_data(ticker)
     
+    option_data = option_data[option_data['open_interest'] != 0.0]
     # Dropping spot price decimals
     spot_price_no_decimals = int(spot_price)
     strikes = [spot_price_no_decimals + i for i in range(-num_of_strikes, num_of_strikes + 1)]
 
-    gamma_values = {}
+    GEX_values = {}
 
     for strike in strikes:
-        call_gamma_weighted = (option_data[(option_data['type'] == 'C') & (option_data['strike'] == strike)]['gamma'] * 
+        call_gamma_weighted = (option_data[(option_data['type'] == 'C') & (option_data['strike'] == strike)]['gamma'] *
+                               option_data[(option_data['type'] == 'C') & (option_data['strike'] == strike)]['delta'] *
+                               spot_price *
                                option_data[(option_data['type'] == 'C') & (option_data['strike'] == strike)]['open_interest']).sum()
         
-        put_gamma_weighted = (option_data[(option_data['type'] == 'P') & (option_data['strike'] == strike)]['gamma'] * 
+        put_gamma_weighted = (option_data[(option_data['type'] == 'P') & (option_data['strike'] == strike)]['gamma'] *
+                              option_data[(option_data['type'] == 'P') & (option_data['strike'] == strike)]['delta'] *
+                              spot_price *
                               option_data[(option_data['type'] == 'P') & (option_data['strike'] == strike)]['open_interest']).sum()
 
-        total_weighted_gamma = call_gamma_weighted - put_gamma_weighted
-        gamma_values[strike] = {'Total': total_weighted_gamma, 'C': call_gamma_weighted, 'P': put_gamma_weighted * -1}
+        total_GEX = call_gamma_weighted + put_gamma_weighted
+        GEX_values[strike] = {'Total': int(total_GEX), 'C': int(call_gamma_weighted), 'P': int(put_gamma_weighted)}
 
-    return gamma_values
+    return GEX_values
+
 
 def plot_gamma_results(gamma_results):
-    strikes = []
-    total_gammas = []
+    # Filter out results where total gamma is zero.
+    filtered_results = {strike: gamma for strike, gamma in gamma_results.items() if gamma['Total'] != 0}
 
-    for strike, gamma in gamma_results.items():
-        strikes.append(strike)
-        total_gammas.append(gamma['Total'])
+    strikes = list(filtered_results.keys())
+    total_gammas = [gamma['Total'] for gamma in filtered_results.values()]
 
     # Plotting
     plt.figure(figsize=(12,7))
@@ -81,12 +86,16 @@ def plot_gamma_results(gamma_results):
     # Color bars and add text label
     for bar in bars:
         yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width()/2, yval + 5, round(yval, 2), ha='center', va='bottom')
+        if yval != 0:  # Only add a label if the gamma value is not 0.
+            plt.text(bar.get_x() + bar.get_width()/2, yval + 5, round(yval, 2), ha='center', va='bottom')
         if yval > 0:
             bar.set_color('green')
         else:
             bar.set_color('red')
 
+    # Adding a horizontal line at y=0
+    plt.axhline(y=0, color='black', linestyle='--')
+    
     # Labeling
     plt.title('Total Gamma Results by Strike')
     plt.xlabel('Strike')
@@ -99,7 +108,7 @@ def plot_gamma_results(gamma_results):
 ticker = 'SPY'
 num_of_strikes = 5
 
-gamma_results = compute_weighted_gamma(ticker, num_of_strikes)
+gamma_results = compute_GEX(ticker, num_of_strikes)
 
 print('GEX at each strike:')
 for strike, gamma in gamma_results.items():
@@ -109,3 +118,4 @@ for strike, gamma in gamma_results.items():
     print(f"\t\tP: {int(gamma['P'])}")
 
 plot_gamma_results(gamma_results)
+    
